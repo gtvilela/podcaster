d2e7b05b63389f9c6f221a0a3447e35c3932c40c
Gabriela Vilela <gabrielathalitasv@gmail.com>
2021-04-21 16:41:13 -0300

    chore: aprendendo um pouco sobre getStaticProps e Link do next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
-import React, { useEffect } from "react";
+import { GetStaticProps } from "next";
+import Image from "next/image";
+import { format, parseISO } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
-export default function Home(props) {
+import styles from "./home.module.scss";
+import Link from "next/link";
 
+type Episodes = {
+  id: string;
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+};
+
+type HomeProps = {
+  latestEpisodes: Episodes[];
+  allEpisodes: Episodes[];
+};
+
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   // Modo SPA - Utilizando o hook do React, porém o brownser não espera a chamada da API para indexação
   // useEffect(() => {
   //   fetch('http://localhost:3333/episodes')
@@ -10,33 +33,135 @@ export default function Home(props) {
   // }, [])
 
   return (
-    <div>
-      <h1>Index</h1>
-      <span>{JSON.stringify(props.episodes)}</span>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+
+        <ul>
+          {latestEpisodes.map((ep) => {
+            return (
+              <li key={ep.id}>
+                <Image
+                  src={ep.thumbnail}
+                  alt={ep.title}
+                  height={192}
+                  width={192}
+                  objectFit="cover"
+                />
+
+                <div className={styles.episodeDetails}>
+                  <Link href={`/episodes/${ep.id}`}>
+                    <a>{ep.title}</a>
+                  </Link>
+                  <p>{ep.members}</p>
+                  <span>{ep.publishedAt}</span>
+                  <span>{ep.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar episódio" />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <section className={styles.allEpisodes}>
+        <h2>Todos episódios</h2>
+        <table cellSpacing={0}>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allEpisodes.map((ep) => {
+              return (
+                <tr key={ep.id}>
+                  <td style={{ width: 72 }}>
+                    <Image
+                      width={120}
+                      height={120}
+                      src={ep.thumbnail}
+                      alt={ep.title}
+                      objectFit="cover"
+                    />
+                  </td>
+                  <td> 
+                    <Link href={`/episodes/${ep.id}`}>
+                      <a>{ep.title}</a>
+                    </Link>
+                  </td>
+                  <td>{ep.members}</td>
+                  <td style={{ width: 100 }}>{ep.publishedAt}</td>
+                  <td>{ep.durationAsString}</td>
+                  <td>
+                    <button type="button">
+                      <img src="/play-green.svg" alt="Tocar Episódio" />
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
     </div>
-  )
+  );
 }
 
 //Modo SSG - Só funciona em producão, ou seja, tem que buildar o app para ver o SSG funionando
 // Modo SSG - Você pode programar de quanto em quanto tempo a api é chamada novamene
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
-  
-  return { props: {
-        episodes: data,
-      },
-      revalidate: 60 * 60 * 8,
-    }
-  }
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      url: episode.file.url,
+    };
+  });
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
+
+  return {
+    props: {
+      latestEpisodes,
+      allEpisodes,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+};
 
 //Modo SSR - Chama a api da função toda vez que o usuário acessar essa página
-// export async function getServerSideProps() {
+// export async function getServerSideProps(): GetServerSideProps {
 // const response = await fetch('http://localhost:3333/episodes')
 // const data = await response.json()
 
 // return { props: {
 //       episodes: data,
-//     } 
+//     }
 //   }
 // }
